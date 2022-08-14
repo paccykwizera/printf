5c03e858721b0d11618fbd3d60e845cabaaeb1f9
Sethmoise7 <106748822+Sethmoise7@users.noreply.github.com>
2022-08-14 23:18:52 +0200

    Update print_edgecase.c

--- print_edgecase.c
+++ print_edgecase.c
@@ -4,7 +4,7 @@
 /**
  * print_pointer - Prints the value of a pointer variable
  * to the
- * standard output
+ * standard output.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
